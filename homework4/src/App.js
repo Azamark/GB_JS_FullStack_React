6ab22352700f8276dd8a370c289cdf19252c61c4
MGainullin <ya.azamark@yandex.ru>
2022-11-13 19:17:30 +0300

    Init MUI

---
+++ homework4/src/App.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import { Message } from './components/Message.jsx'
+import { Form } from './components/Form.jsx'
+
+function App() {
+  const [messageList, setMessageList] = useState([]);
+  const [message, setMessage] = useState({
+    author: '',
+    text: '',
+    id: Date.now()
+  });
+
+  useEffect(() => {
+    if (messageList.length > 0 && messageList.slice(-1)[0].author !== 'robot') {
+      setTimeout(() => {
+        setMessageList(prevstate => [...prevstate, { text: 'Привет! Я робот!', author: 'robot' }])
+      }, 1500)
+    }
+  }, [messageList]);
+
+
+  return (
+    <div className="App">
+      <div className="container flex">
+        <h1>HomeWork4</h1>
+        <Form data={message} setData={setMessage} setMessage={setMessageList} />
+        <h2>
+          Сообщения:
+        </h2>
+        <div className='messageList'>
+          {
+            messageList.map((e, i) => <Message author={e.author} text={e.text} key={e.id} />)
+          }
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
